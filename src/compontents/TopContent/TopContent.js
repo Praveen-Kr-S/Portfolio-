38bc6ce2957c8e8cfa005096fb836a113a5e3d8d
Praveen-Kr-S <praveenkumar328be@gmail.com>
2024-01-13 03:21:17 +0530

    update app for mobile responsible

--- src/compontents/TopContent/TopContent.js
+++ src/compontents/TopContent/TopContent.js
@@ -7,7 +7,9 @@ const TopContent = () => {
     <div className='topContent'>
         <div className='topContent__container'>
             <h1><span className='heading__span1'>Mr.</span>Praveen Kumar</h1>
-            <p>Qualified Front End Developer excited at the prospect of collaborating with team members and improving on ui in a professional setting. Creative worker recognized by peers for my effective creative thinking and motivational skills. Looking to apply my understanding of JavaScript and Application Development at your company. </p>
+            <p>
+              Qualified Front End Developer excited at the prospect of collaborating with team members and improving on ui in a professional setting. Creative worker recognized by peers for my effective creative thinking and motivational skills. Looking to apply my understanding of JavaScript and Application Development at your company.
+            </p>
                 <button className='topContent__downloadButton'>
                   <a href="https://www.linkedin.com/in/praveen-kumar-s-2b6315244/">Download CV</a>
                 </button>
